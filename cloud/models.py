61d8e9a1397511507a45b4220f2ae0a9bc00391c
bhagyashree <bhagyashree.goyal2@gmail.com>
2016-07-06 15:17:19 +0530

    My first Django Docker App,first commit

---
+++ cloud/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.utils import timezone
+# Create your models here.
+class Docker(models.Model):
+	name = models.CharField(max_length=30)
+	password = models.CharField(max_length=50)
+	count_of_containers = models.IntegerField() 
+	choose_language = models.CharField(max_length=30)
+	code = models.TextField()
+#	container_id = models.ForeignKey('id.Container')
+
+def __str__(self):
+	return self.name
